1bf744db3b2e6473f4a03c55846c2cda5950b412
Martin Reinhardt <contact@martinreinhardt-online.de>
2015-06-19 11:15:07 +0200

    update galen presentation enterJS 2015

--- presentations/enterjs_galen/code/test/bootstrap/runGalenTests.sh
+++ presentations/enterjs_galen/code/test/bootstrap/runGalenTests.sh
@@ -3,6 +3,5 @@
 currentDir=$( pwd )
 websiteUrl='http://getbootstrap.com'
 
-galen test homePage.test -DwebsiteUrl=${websiteUrl} --htmlreport ../../reports/bootstrap/home
-galen test cssPage.test -DwebsiteUrl=${websiteUrl}'/css' --htmlreport ../../reports/bootstrap/css
-galen test jsPage.test -DwebsiteUrl=${websiteUrl}'/javascript' --htmlreport ../../reports/bootstrap/javascript
+galen test basic.test -DwebsiteUrl=${websiteUrl} --htmlreport ../../reports/bootstrap -Dwebdriver.chrome.driver=/opt/dev/chromedriver
+open  ../../reports/bootstrap/report.html
